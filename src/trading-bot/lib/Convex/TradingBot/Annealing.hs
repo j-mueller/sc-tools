c19e8700ca4f2b76c4b0676031f5041bc8b67e9b
Jann Müller <j-mueller@users.noreply.github.com>
2022-11-29 10:46:03 +0100

    Show % value change

--- src/trading-bot/lib/Convex/TradingBot/Annealing.hs
+++ src/trading-bot/lib/Convex/TradingBot/Annealing.hs
@@ -95,19 +95,19 @@ runBacktestNode rule logEnv Config{cardanoNodeConfigFile, cardanoNodeSocket} = d
       logInfoS "Backtesting finished"
       liftIO (STM.atomically (STM.takeTMVar tv))
 
-runBacktest :: SearchableRule r => FilePath -> r -> (ExceptT CsvParseException IO) (Portfolio, C.Value)
-runBacktest fp rule =
+runBacktest :: SearchableRule r => Lovelace -> FilePath -> r -> (ExceptT CsvParseException IO) (Portfolio, C.Value)
+runBacktest initialAda fp rule =
   let options = defaultDecodeOptions
   in withBinaryFileContents fp
-      $ evalPriceEvents rule
+      $ evalPriceEvents initialAda rule
       . decodeByNameWith options
 
 pricesFor :: (PolicyId, AssetName) -> Lens' Prices LPPrices
 pricesFor k = priceHistory . at k . anon Prices.empty Prices.null
 
-evalPriceEvents :: (SearchableRule r, Monad m) => r -> Stream (Of PriceEventRow) m () -> m (Portfolio, C.Value)
-evalPriceEvents (signal -> rule) stream = do
-  let portf = (Portfolio.emptyPortfolio, C.lovelaceToValue 3_000_000_000)
+evalPriceEvents :: (SearchableRule r, Monad m) => Lovelace -> r -> Stream (Of PriceEventRow) m () -> m (Portfolio, C.Value)
+evalPriceEvents initialAda (signal -> rule) stream = do
+  let portf = (Portfolio.emptyPortfolio, C.lovelaceToValue initialAda)
       init_ :: Prices
       init_ = Prices mempty mempty
   flip evalStateT init_ $ Portfolio.execSimulatedPortfolioT defaultPortfolioConfig portf $ do
@@ -130,9 +130,9 @@ evalPriceEvents (signal -> rule) stream = do
 
 {-| Evaluate the rule against the price events in the CSV file
 -}
-evaluateRule :: SearchableRule r => FilePath -> K.LogEnv -> r -> IO (Portfolio, C.Value)
-evaluateRule csvFile logEnv rule = K.runKatipContextT logEnv () "test-rule" $ runMonadLogKatipT $ do
-  liftIO $ runExceptT (runBacktest csvFile rule) >>= either (error . show) pure
+evaluateRule :: SearchableRule r => Lovelace -> FilePath -> K.LogEnv -> r -> IO (Portfolio, C.Value)
+evaluateRule initialAda csvFile logEnv rule = K.runKatipContextT logEnv () "test-rule" $ runMonadLogKatipT $ do
+  liftIO $ runExceptT (runBacktest initialAda csvFile rule) >>= either (error . show) pure
 
 fitness :: (Portfolio, C.Value) -> Double
 fitness (portfolio, vl) =
@@ -141,34 +141,39 @@ fitness (portfolio, vl) =
       then (1 / fromIntegral totalVal)
       else 1
 
-optimiseRules :: forall r m. (SearchableRule r, MonadIO m) => r -> FilePath -> K.LogEnv -> m (Stream (Of (AnnealingState (AssessedCandidate r (Portfolio, C.Value)))) IO (), AssessedCandidate r (Portfolio, C.Value))
+optimiseRules :: forall r m. (SearchableRule r, MonadIO m) => r -> FilePath -> K.LogEnv -> m (Stream (Of (AnnealingState (AssessedCandidate r (Portfolio, C.Value)))) IO (), AssessedCandidate r (Portfolio, C.Value), Lovelace)
 optimiseRules i fp logEnv = do
   gen <- liftIO P.createSystemRandom
-  initial <- liftIO (evaluateRule fp logEnv i)
-  let temp j = 1 + (50 / fromIntegral j)
+  let initialAda = 3_000_000_000
+  initial <- liftIO (evaluateRule initialAda fp logEnv i)
+  let temp j = 1 + (1 / fromIntegral j)
       cand = AssessedCandidate i initial (fitness initial)
       stream =
         annealing
           gen
           temp
-          (evaluateRule fp logEnv)
+          (evaluateRule initialAda fp logEnv)
           fitness
           cand
           (neighbours gen)
-  return (stream, cand)
+  return (stream, cand, initialAda)
 
 runAnnealing :: (MonadIO m, MonadLog m) => K.LogEnv -> FilePath -> m ()
 runAnnealing logEnv filePath = do
   logInfoS "Starting annealing"
-  let r = Rules.movingAverages
-  (stream, initial) <- optimiseRules r filePath logEnv
-  result <- liftIO (S.last_ (S.take 0_100 stream))
+  let r = Rules.twoMovingAverages
+  (stream, initialRule, C.Lovelace initialAda) <- optimiseRules r filePath logEnv
+  result <- liftIO (S.last_ (S.take 0_500 stream))
   flip traverse_ result $ \AnnealingState{asBestCandidate, asCurrentCandidate} -> do
     let p AssessedCandidate{acCandidate, acResult=(pf, v)} = do
           logInfoS (show acCandidate)
+          let C.Lovelace currentVal = Portfolio.aum v pf
+              diff = currentVal - initialAda
+              percent = (100 :: Double) * (fromIntegral diff / fromIntegral initialAda)
           Portfolio.printPortfolioInfo v pf
+          logInfoS $ "Value change: " <> show percent <> "%"
     logInfoS "Initial value:"
-    p initial
+    p initialRule
 
     logInfoS $ "Best result:"
     p asBestCandidate
