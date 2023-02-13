e0f4591cdd159a66301405e9dc93d2d58aa5ecf2
Jann Müller <j-mueller@users.noreply.github.com>
2023-02-13 10:00:19 +0100

    devnet: Add function for creating a seeded wallet

--- src/devnet/test/Spec.hs
+++ src/devnet/test/Spec.hs
@@ -10,20 +10,19 @@ import           Control.Monad.Except      (runExceptT)
 import           Convex.Devnet.CardanoNode (NodeLog, RunningNode (..),
                                             getCardanoNodeVersion,
                                             withCardanoNodeDevnet)
-import           Convex.Devnet.Logging     (contramap, showLogsOnFailure,
-                                            traceWith)
-import           Convex.Devnet.NodeQueries (loadConnectInfo, waitForTxn)
+import           Convex.Devnet.Logging     (contramap, showLogsOnFailure)
+import           Convex.Devnet.NodeQueries (loadConnectInfo)
 import           Convex.Devnet.Utils       (failAfter, failure, withTempDir)
 import           Convex.Devnet.Wallet      (WalletLog)
 import qualified Convex.Devnet.Wallet      as W
-import qualified Convex.Wallet             as W
+import qualified Convex.Utxos              as Utxos
 import           Data.Aeson                (FromJSON, ToJSON)
 import           Data.List                 (isInfixOf)
 import qualified Data.Text                 as Text
 import           GHC.Generics              (Generic)
 import           GHC.IO.Encoding           (setLocaleEncoding, utf8)
 import           Test.Tasty                (defaultMain, testGroup)
-import           Test.Tasty.HUnit          (assertBool, testCase)
+import           Test.Tasty.HUnit          (assertBool, assertEqual, testCase)
 
 main :: IO ()
 main = do
@@ -54,13 +53,10 @@ makePayment = do
   showLogsOnFailure $ \tr -> do
     failAfter 10 $
       withTempDir "cardano-cluster" $ \tmp -> do
-        withCardanoNodeDevnet (contramap TLNode tr) tmp $ \runningNode@RunningNode{rnNodeSocket, rnNetworkId} -> do
-          wllt <- W.generateWallet
-          tx <- W.sendFaucetFundsTo (contramap TLWallet tr) runningNode (W.addressInEra rnNetworkId wllt) 100_000_000
-          let txi = C.getTxId (C.getTxBody tx)
-          traceWith tr (SubmitTx txi)
-          waitForTxn rnNetworkId rnNodeSocket tx
-          traceWith tr (FoundTx txi)
+        withCardanoNodeDevnet (contramap TLNode tr) tmp $ \runningNode -> do
+          wllt <- W.createSeededWallet (contramap TLWallet tr) runningNode 100_000_000
+          bal <- Utxos.totalBalance <$> W.walletUtxos runningNode wllt
+          assertEqual "Wallet should have the expected balance" 100_000_000 (C.selectLovelace bal)
 
 data TestLog =
   TLWallet WalletLog | TLNode NodeLog | SubmitTx{ txId :: C.TxId } | FoundTx{txId :: C.TxId }
