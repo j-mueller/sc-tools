73290f4e1e5bf905c8bf7173c1e74a51e6f0b945
Jann Müller <j-mueller@users.noreply.github.com>
2023-10-16 14:31:08 +0200

    mockchain: Make it easier to change the node / protocol parameters

--- src/mockchain/lib/Convex/NodeParams.hs
+++ src/mockchain/lib/Convex/NodeParams.hs
@@ -7,12 +7,42 @@ module Convex.NodeParams(
   systemStart,
   eraHistory,
   stakePools,
-  slotLength
+  slotLength,
+
+  -- * Lenses for @ProtocolParameters@
+  -- See https://input-output-hk.github.io/cardano-node/cardano-api/lib/Cardano-Api-Shelley.html#t:ProtocolParameters
+  -- for an explanation of the fields
+  protocolVersion,
+  decentralization,
+  extraPraosEntropy,
+  maxBlockHeaderSize,
+  maxBlockBodySize,
+  maxTxSize,
+  txFeeFixed,
+  txFeeFerByte,
+  minUTxOValue,
+  stakeAddressDeposit,
+  stakePoolDeposit,
+  minPoolCost,
+  poolRetireMaxEpoch,
+  stakePoolTargetNum,
+  poolPledgeInfluence,
+  monetaryExpansion,
+  treasuryCut,
+  uTxOCostPerWord,
+  costModels,
+  prices,
+  maxTxExUnits,
+  maxBlockExUnits,
+  maxValueSize,
+  collateralPercent,
+  maxCollateralInputs,
+  uTxOCostPerByte
 ) where
 
 import           Cardano.Api           (BabbageEra, BundledProtocolParameters)
 import           Cardano.Api.Shelley   (CardanoMode, EraHistory, NetworkId (..),
-                                        PoolId)
+                                        PoolId, ProtocolParameters (..))
 import           Cardano.Slotting.Time (SlotLength, SystemStart)
 import           Control.Lens.TH       (makeLensesFor)
 import           Data.Set              as Set (Set)
@@ -35,3 +65,32 @@ makeLensesFor
   , ("npStakePools", "stakePools")
   , ("npSlotLength", "slotLength")
   ] ''NodeParams
+
+makeLensesFor
+  [ ("protocolParamProtocolVersion", "protocolVersion")
+  , ("protocolParamDecentralization", "decentralization")
+  , ("protocolParamExtraPraosEntropy", "extraPraosEntropy")
+  , ("protocolParamMaxBlockHeaderSize", "maxBlockHeaderSize")
+  , ("protocolParamMaxBlockBodySize", "maxBlockBodySize")
+  , ("protocolParamMaxTxSize", "maxTxSize")
+  , ("protocolParamTxFeeFixed", "txFeeFixed")
+  , ("protocolParamTxFeePerByte", "txFeeFerByte")
+  , ("protocolParamMinUTxOValue", "minUTxOValue")
+  , ("protocolParamStakeAddressDeposit", "stakeAddressDeposit")
+  , ("protocolParamStakePoolDeposit", "stakePoolDeposit")
+  , ("protocolParamMinPoolCost", "minPoolCost")
+  , ("protocolParamPoolRetireMaxEpoch", "poolRetireMaxEpoch")
+  , ("protocolParamStakePoolTargetNum", "stakePoolTargetNum")
+  , ("protocolParamPoolPledgeInfluence", "poolPledgeInfluence")
+  , ("protocolParamMonetaryExpansion", "monetaryExpansion")
+  , ("protocolParamTreasuryCut", "treasuryCut")
+  , ("protocolParamUTxOCostPerWord", "uTxOCostPerWord")
+  , ("protocolParamCostModels", "costModels")
+  , ("protocolParamPrices", "prices")
+  , ("protocolParamMaxTxExUnits", "maxTxExUnits")
+  , ("protocolParamMaxBlockExUnits", "maxBlockExUnits")
+  , ("protocolParamMaxValueSize", "maxValueSize")
+  , ("protocolParamCollateralPercent", "collateralPercent")
+  , ("protocolParamMaxCollateralInputs", "maxCollateralInputs")
+  , ("protocolParamUTxOCostPerByte", "uTxOCostPerByte")
+  ] ''ProtocolParameters
