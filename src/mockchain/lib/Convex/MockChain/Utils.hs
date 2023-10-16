8c7b30529c5453ece17d1184ba73deb493fb8482
Jann Müller <j-mueller@users.noreply.github.com>
2023-10-16 14:31:08 +0200

    mockchain: Make it easier to change the node / protocol parameters

--- src/mockchain/lib/Convex/MockChain/Utils.hs
+++ src/mockchain/lib/Convex/MockChain/Utils.hs
@@ -1,28 +1,43 @@
 {-# LANGUAGE LambdaCase #-}
 module Convex.MockChain.Utils(
   mockchainSucceeds,
-  mockchainFails
+  mockchainSucceedsWith,
+  mockchainFails,
+  mockchainFailsWith
   ) where
 
-import           Convex.MockChain         (Mockchain, MockchainError,
-                                           MockchainIO, runMockchain0,
-                                           runMockchain0IO)
-import qualified Convex.Wallet.MockWallet as Wallet
-import           Test.HUnit               (Assertion)
+import           Convex.MockChain          (MockchainError, MockchainIO,
+                                            runMockchain0IOWith)
+import qualified Convex.MockChain.Defaults as Defaults
+import           Convex.NodeParams         (NodeParams)
+import qualified Convex.Wallet.MockWallet  as Wallet
+import           Test.HUnit                (Assertion)
 
 {-| Run the 'Mockchain' action and fail if there is an error
 -}
 mockchainSucceeds :: MockchainIO a -> Assertion
-mockchainSucceeds action =
-  runMockchain0IO Wallet.initialUTxOs action >>= \case
+mockchainSucceeds = mockchainSucceedsWith Defaults.nodeParams
+
+{-| Run the 'Mockchain' action with the given node parameters and fail if there is an error
+-}
+mockchainSucceedsWith :: NodeParams -> MockchainIO a -> Assertion
+mockchainSucceedsWith params action =
+   runMockchain0IOWith Wallet.initialUTxOs params action >>= \case
     Right{}  -> pure ()
     Left err -> fail (show err)
 
 {-| Run the 'Mockchain' action, fail if it succeeds, and handle the error
   appropriately.
-|-}
-mockchainFails :: Mockchain a -> (MockchainError -> Assertion) -> Assertion
-mockchainFails action handleError =
-  case runMockchain0 Wallet.initialUTxOs action of
-    Right _  -> fail ("mockchainFails: Did not fail")
+-}
+mockchainFails :: MockchainIO a -> (MockchainError -> Assertion) -> Assertion
+mockchainFails action =
+  mockchainFailsWith Defaults.nodeParams action
+
+{-| Run the 'Mockchain' action with the given node parameters, fail if it
+    succeeds, and handle the error appropriately.
+-}
+mockchainFailsWith :: NodeParams -> MockchainIO a -> (MockchainError -> Assertion) -> Assertion
+mockchainFailsWith params action handleError =
+  runMockchain0IOWith Wallet.initialUTxOs params action >>= \case
+    Right _  -> fail ("mockchainFailsWith: Did not fail")
     Left err -> handleError err
--- src/mockchain/lib/Convex/NodeParams.hs
+++ src/mockchain/lib/Convex/NodeParams.hs
@@ -8,7 +8,37 @@ module Convex.NodeParams(
   systemStart,
   eraHistory,
   stakePools,
-  slotLength
+  slotLength,
+
+  -- * Lenses for @ProtocolParameters@
+  -- See https://input-output-hk.github.io/cardano-node/cardano-api/lib/Cardano-Api-Shelley.html#t:ProtocolParameters
+  -- for an explanation of the fields
+  protocolVersion,
+  decentralization,
+  extraPraosEntropy,
+  maxBlockHeaderSize,
+  maxBlockBodySize,
+  maxTxSize,
+  txFeeFixed,
+  txFeeFerByte,
+  minUTxOValue,
+  stakeAddressDeposit,
+  stakePoolDeposit,
+  minPoolCost,
+  poolRetireMaxEpoch,
+  stakePoolTargetNum,
+  poolPledgeInfluence,
+  monetaryExpansion,
+  treasuryCut,
+  uTxOCostPerWord,
+  costModels,
+  prices,
+  maxTxExUnits,
+  maxBlockExUnits,
+  maxValueSize,
+  collateralPercent,
+  maxCollateralInputs,
+  uTxOCostPerByte
 ) where
 
 import           Cardano.Api.Shelley            (CardanoMode, EraHistory,
@@ -40,3 +70,32 @@ makeLensesFor
   , ("npSlotLength", "slotLength")
   , ("npLedgerParams", "ledgerProtocolParameters")
   ] ''NodeParams
+
+makeLensesFor
+  [ ("protocolParamProtocolVersion", "protocolVersion")
+  , ("protocolParamDecentralization", "decentralization")
+  , ("protocolParamExtraPraosEntropy", "extraPraosEntropy")
+  , ("protocolParamMaxBlockHeaderSize", "maxBlockHeaderSize")
+  , ("protocolParamMaxBlockBodySize", "maxBlockBodySize")
+  , ("protocolParamMaxTxSize", "maxTxSize")
+  , ("protocolParamTxFeeFixed", "txFeeFixed")
+  , ("protocolParamTxFeePerByte", "txFeeFerByte")
+  , ("protocolParamMinUTxOValue", "minUTxOValue")
+  , ("protocolParamStakeAddressDeposit", "stakeAddressDeposit")
+  , ("protocolParamStakePoolDeposit", "stakePoolDeposit")
+  , ("protocolParamMinPoolCost", "minPoolCost")
+  , ("protocolParamPoolRetireMaxEpoch", "poolRetireMaxEpoch")
+  , ("protocolParamStakePoolTargetNum", "stakePoolTargetNum")
+  , ("protocolParamPoolPledgeInfluence", "poolPledgeInfluence")
+  , ("protocolParamMonetaryExpansion", "monetaryExpansion")
+  , ("protocolParamTreasuryCut", "treasuryCut")
+  , ("protocolParamUTxOCostPerWord", "uTxOCostPerWord")
+  , ("protocolParamCostModels", "costModels")
+  , ("protocolParamPrices", "prices")
+  , ("protocolParamMaxTxExUnits", "maxTxExUnits")
+  , ("protocolParamMaxBlockExUnits", "maxBlockExUnits")
+  , ("protocolParamMaxValueSize", "maxValueSize")
+  , ("protocolParamCollateralPercent", "collateralPercent")
+  , ("protocolParamMaxCollateralInputs", "maxCollateralInputs")
+  , ("protocolParamUTxOCostPerByte", "uTxOCostPerByte")
+  ] ''ProtocolParameters
