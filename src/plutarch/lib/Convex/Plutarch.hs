0ff219d06a8d27d7936e695cb9b4a1746c61a125
Jann Müller <j-mueller@users.noreply.github.com>
2024-02-18 21:56:57 +0100

    Plutarch interop

---
+++ src/plutarch/lib/Convex/Plutarch.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE TypeOperators       #-}
+{-| Plutarch / cardano-api interop
+-}
+module Convex.Plutarch(
+  CardanoApiScriptError(..),
+  plutarchScriptToCapiScript,
+  wrapValidator,
+) where
+
+import qualified Cardano.Api.Shelley    as C
+import           Data.Text              (Text)
+import           Plutarch               (pcon, popaque, (#))
+import           Plutarch.Api.V1        (PScriptContext)
+import           Plutarch.Internal      (punsafeCoerce)
+import           Plutarch.Prelude       (PBool, PData, PIsData,
+                                         POpaque, PUnit (..), Term,
+                                         pfromData, pif, plam, ptraceError,
+                                         type (:-->))
+import           Plutarch.Script        (Script (..))
+import           PlutusLedgerApi.Common (serialiseUPLC)
+
+data CardanoApiScriptError =
+    PlutarchScriptError Text
+    deriving Show
+
+wrapValidator ::
+    (PIsData dt, PIsData rdmr) =>
+    Term s (dt :--> rdmr :--> PScriptContext :--> PBool) ->
+    Term s (PData :--> PData :--> PScriptContext :--> POpaque)
+wrapValidator validator = plam $ \datum redeemer ctx ->
+    let dt = pfromData $ punsafeCoerce datum
+        rdmr = pfromData $ punsafeCoerce redeemer
+        result = validator # dt # rdmr # ctx
+     in popaque $ pif result (pcon PUnit) (ptraceError "Validator reduced to False")
+
+plutarchScriptToCapiScript :: Script -> C.PlutusScript C.PlutusScriptV2
+plutarchScriptToCapiScript (Script k) = C.PlutusScriptSerialised $ serialiseUPLC k
