8850a419ab443c19b7d2e0e9edb326e3056edb55
edgr <edgr@pm.me>
2024-06-20 15:40:13 +0200

    Added stake related tests + test helper

--- src/coin-selection/test/Spec.hs
+++ src/coin-selection/test/Spec.hs
@@ -33,7 +33,7 @@ import           Convex.Class                   (MonadBlockchain (..),
                                                  MonadMockchain,
                                                  MonadDatumQuery (queryDatumFromHash),
                                                  SendTxFailed (..), getUtxo,
-                                                 setUtxo, singleUTxO)
+                                                 setUtxo, singleUTxO, setReward)
 import           Convex.CoinSelection           (BalanceTxError, keyWitnesses,
                                                  publicKeyCredential)
 import qualified Convex.CardanoApi.Lenses                  as L
@@ -65,6 +65,7 @@ import           Data.Foldable                  (traverse_)
 import           Data.List.NonEmpty             (NonEmpty (..))
 import qualified Data.Map                       as Map
 import qualified Data.Set                       as Set
+import           Data.Ratio                     ((%))
 import qualified PlutusLedgerApi.V2             as PV2
 import qualified Scripts
 import qualified Test.QuickCheck.Gen            as Gen
@@ -106,6 +107,8 @@ tests = testGroup "unit tests"
   , testGroup "staking"
     [ testCase "register a staking credential" (mockchainSucceeds $ failOnError registerStakingCredential)
     , testCase "zero withdrawal" (mockchainSucceeds $ failOnError $ registerStakingCredential >> withdrawZero)
+    , testCase "query stake addresses" (mockchainSucceeds $ failOnError queryStakeAddressesTest)
+    , testCase "withdrawal" (mockchainSucceeds $ failOnError withdrawalTest)
     ]
   ]
 
@@ -392,3 +395,146 @@ matchingIndexMP = do
       policyId = C.PolicyId sh
       runTx assetName = Scripts.mintMatchingIndex policyId assetName 100
   void $ tryBalanceAndSubmit mempty Wallet.w1 (execBuildTx $ traverse_ runTx ["assetName1", "assetName2", "assetName3"]) []
+
+queryStakeAddressesTest :: forall m. (MonadIO m, MonadMockchain m, MonadError BalanceTxError m, MonadFail m) => m ()
+queryStakeAddressesTest = withRegisteredPool Wallet.w2 $ \ poolId -> do
+  stakeKey <- C.generateSigningKey C.AsStakeKey
+  let
+    withdrawalAmount = 10_000_000
+
+    stakeHash =
+      C.verificationKeyHash . C.getVerificationKey $ stakeKey
+
+    stakeCred = C.StakeCredentialByKey stakeHash
+
+    stakeCert =
+      C.makeStakeAddressRegistrationCertificate
+      . C.StakeAddrRegistrationPreConway C.ShelleyToBabbageEraBabbage
+      $ stakeCred
+
+    delegationCert =
+      C.makeStakeAddressDelegationCertificate
+      $ C.StakeDelegationRequirementsPreConway C.ShelleyToBabbageEraBabbage stakeCred poolId
+
+    stakeCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate stakeCert
+
+    delegCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate delegationCert
+
+  -- activate stake
+  void $ tryBalanceAndSubmit mempty Wallet.w2 stakeCertTx []
+  -- delegate to pool 
+  void $ tryBalanceAndSubmit mempty Wallet.w2 delegCertTx [C.WitnessStakeKey stakeKey]
+
+  -- modify the ledger state
+  setReward stakeCred (C.quantityToLovelace withdrawalAmount)
+
+  (rewards, delegations) <- queryStakeAddresses (Set.fromList [stakeCred]) Defaults.networkId
+
+  when (length rewards /= 1) $ fail "Expected 1 reward"
+  when (length delegations /= 1) $ fail "Expected 1 delegation"
+
+withdrawalTest :: forall m. (MonadIO m, MonadMockchain m, MonadError BalanceTxError m, MonadFail m) => m ()
+withdrawalTest = withRegisteredPool Wallet.w1 $ \ poolId -> do
+  stakeKey <- C.generateSigningKey C.AsStakeKey
+  let
+    withdrawalAmount = 10_000_000
+
+    stakeHash =
+      C.verificationKeyHash . C.getVerificationKey $ stakeKey
+
+    stakeCred = C.StakeCredentialByKey stakeHash
+
+    stakeCert =
+      C.makeStakeAddressRegistrationCertificate
+      . C.StakeAddrRegistrationPreConway C.ShelleyToBabbageEraBabbage
+      $ stakeCred
+    stakeAddress = C.makeStakeAddress Defaults.networkId stakeCred
+
+    delegationCert =
+      C.makeStakeAddressDelegationCertificate
+      $ C.StakeDelegationRequirementsPreConway C.ShelleyToBabbageEraBabbage stakeCred poolId
+
+    stakeCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate stakeCert
+
+    delegCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate delegationCert
+
+    withdrawalTx = execBuildTx $ do
+      BuildTx.addWithdrawal stakeAddress withdrawalAmount (C.KeyWitness C.KeyWitnessForStakeAddr)
+
+  -- activate stake
+  void $ tryBalanceAndSubmit mempty Wallet.w2 stakeCertTx []
+  -- delegate to pool 
+  void $ tryBalanceAndSubmit mempty Wallet.w2 delegCertTx [C.WitnessStakeKey stakeKey]
+
+  -- modify the ledger state
+  setReward stakeCred (C.quantityToLovelace withdrawalAmount)
+
+  -- withdraw rewards
+  void $ tryBalanceAndSubmit mempty Wallet.w2 withdrawalTx [C.WitnessStakeKey stakeKey]
+
+{-| Run the 'Mockchain' action with registered pool
+-} 
+withRegisteredPool :: forall m a. (MonadIO m, MonadMockchain m, MonadError BalanceTxError m, MonadFail m) => Wallet -> (C.PoolId -> m a) -> m a
+withRegisteredPool wallet f = do
+  stakeKey <- C.generateSigningKey C.AsStakeKey
+  vrfKey <- C.generateSigningKey C.AsVrfKey
+  stakePoolKey <- C.generateSigningKey C.AsStakePoolKey
+
+  let
+    vrfHash =
+      C.verificationKeyHash . C.getVerificationKey $ vrfKey
+
+    stakeHash =
+      C.verificationKeyHash . C.getVerificationKey $ stakeKey
+
+    stakeCred = C.StakeCredentialByKey stakeHash
+
+    stakeCert =
+      C.makeStakeAddressRegistrationCertificate
+      . C.StakeAddrRegistrationPreConway C.ShelleyToBabbageEraBabbage
+      $ stakeCred
+    stakeAddress = C.makeStakeAddress Defaults.networkId stakeCred
+
+    stakePoolVerKey = C.getVerificationKey stakePoolKey
+    poolId = C.verificationKeyHash stakePoolVerKey
+
+    delegationCert =
+      C.makeStakeAddressDelegationCertificate
+      $ C.StakeDelegationRequirementsPreConway C.ShelleyToBabbageEraBabbage stakeCred poolId
+
+    stakePoolParams =
+     C.StakePoolParameters
+       poolId
+       vrfHash
+       340_000_000 -- cost
+       (3 % 100) -- margin
+       stakeAddress
+       0 -- pledge
+       [stakeHash] -- owners
+       [] -- relays
+       Nothing
+
+    poolCert =
+      C.makeStakePoolRegistrationCertificate
+      . C.StakePoolRegistrationRequirementsPreConway C.ShelleyToBabbageEraBabbage
+      . C.toShelleyPoolParams
+      $ stakePoolParams
+  
+    stakeCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate stakeCert
+
+    poolCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate poolCert
+
+    delegCertTx = BuildTx.execBuildTx $ do
+      BuildTx.addCertificate delegationCert
+
+  void $ tryBalanceAndSubmit mempty wallet stakeCertTx []
+  void $ tryBalanceAndSubmit mempty wallet poolCertTx [C.WitnessStakeKey stakeKey, C.WitnessStakePoolKey stakePoolKey]
+  void $ tryBalanceAndSubmit mempty wallet delegCertTx [C.WitnessStakeKey stakeKey]
+
+  f poolId
