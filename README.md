3d4392f8ae2539c20b5ec8a51c30c44abe14254d
Jann Müller <j.mueller.11@alumni.ucl.ac.uk>
2024-10-07 11:57:06 +0200

    Update node build in CI

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ The `main` branch uses the following versions of its major dependencies:
 
 |Name|Version|
 |--|--|
-|`cardano-node`|[9.1.0](https://github.com/IntersectMBO/cardano-node/releases/tag/9.1.0)|
+|`cardano-node`|[9.1.1](https://github.com/IntersectMBO/cardano-node/releases/tag/9.1.1)|
 |`cardano-api`|[9.3.0.0](https://chap.intersectmbo.org/package/cardano-api-9.3.0.0/)|
 |`ghc`|9.6.6|
 |`cabal`|3.10.3.0|
--- src/devnet/test/Spec.hs
+++ src/devnet/test/Spec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE DerivingStrategies #-}
@@ -196,7 +197,7 @@ makePayment = do
           let lovelacePerUtxo = 100_000_000
               numUtxos        = 10
           wllt <- W.createSeededWallet (contramap TLWallet tr) runningNode numUtxos lovelacePerUtxo
-          bal <- Utxos.totalBalance <$> W.walletUtxos runningNode wllt
+          bal <- Utxos.totalBalance <$> W.walletUtxos @C.ConwayEra runningNode wllt
           assertEqual "Wallet should have the expected balance" (fromIntegral numUtxos * lovelacePerUtxo) (C.lovelaceToQuantity $ C.selectLovelace bal)
 
 runWalletServer :: IO ()
@@ -205,7 +206,7 @@ runWalletServer =
     withTempDir "cardano-cluster" $ \tmp -> do
       withCardanoNodeDevnet (contramap TLNode tr) tmp $ \node ->
         withWallet (contramap TWallet tr) tmp node $ \wllt -> do
-          bal <- Utxos.totalBalance <$> getUTxOs wllt
+          bal <- Utxos.totalBalance <$> getUTxOs @C.ConwayEra wllt
           let lovelacePerUtxo = 100_000_000
               numUtxos        = 10 :: Int
           assertEqual "Wallet should have the correct balance" (fromIntegral numUtxos * lovelacePerUtxo) (C.selectLovelace bal)
