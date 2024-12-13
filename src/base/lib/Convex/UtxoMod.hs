612fef04403f033353b4569335bbd4f5dcfc11c4
Jann Müller <j.mueller.11@alumni.ucl.ac.uk>
2024-12-13 09:38:09 +0100

    Fix Convex.UtxoMod

--- src/base/lib/Convex/UtxoMod.hs
+++ src/base/lib/Convex/UtxoMod.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE DeriveAnyClass     #-}
 {-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GADTs              #-}
 {-# LANGUAGE LambdaCase         #-}
 {-# LANGUAGE TupleSections      #-}
 {-| Changing hashes
 -}
 module Convex.UtxoMod(
   replaceHash,
-  tryReplaceHash
+  tryReplaceHash,
+  replaceHashAnyLang,
+  FullTx(..)
 ) where
 
 import           Cardano.Api     (Hash, Script, ScriptHash,
                                   ScriptInAnyLang (..))
 import qualified Cardano.Api     as C
+import           Cardano.Binary  (DecoderError)
 import           Control.Monad   (guard)
-import           Data.Aeson      (FromJSON, ToJSON)
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
-import           Data.Functor    (($>), (<&>))
+import           Data.Functor    (($>))
 import           Data.Map        (Map)
-import qualified Data.Map        as Map
 import           Data.Proxy      (Proxy (..))
 import           GHC.Generics    (Generic)
 
@@ -26,12 +28,9 @@ import           GHC.Generics    (Generic)
 with a new hash. Throws an 'error' if it fails
 -}
 tryReplaceHash ::
-  ( C.SerialiseAsRawBytes (Hash h1)
-  , C.SerialiseAsRawBytes (Hash h2)
-  , C.SerialiseAsRawBytes (Script lang)
-  , C.HasTypeProxy h1
-  , C.HasTypeProxy h2
-  , C.HasTypeProxy lang
+  ( C.SerialiseAsCBOR (Hash h1)
+  , C.SerialiseAsCBOR (Hash h2)
+  , C.IsScriptLanguage lang
   ) => Hash h1 -> Hash h2 -> Script lang -> Script lang
 tryReplaceHash oldHash newHash  =
   either (error . show) id . replaceHash oldHash newHash
@@ -40,19 +39,16 @@ tryReplaceHash oldHash newHash  =
 with a new hash
 -}
 replaceHash ::
-  ( C.SerialiseAsRawBytes (Hash h1)
-  , C.SerialiseAsRawBytes (Hash h2)
-  , C.SerialiseAsRawBytes (Script lang)
-  , C.HasTypeProxy h1
-  , C.HasTypeProxy h2
-  , C.HasTypeProxy lang
-  ) => Hash h1 -> Hash h2 -> Script lang -> Either C.SerialiseAsRawBytesError (Script lang)
+  ( C.SerialiseAsCBOR (Hash h1)
+  , C.SerialiseAsCBOR (Hash h2)
+  , C.IsScriptLanguage lang
+  ) => Hash h1 -> Hash h2 -> Script lang -> Either DecoderError (Script lang)
 replaceHash oldHash newHash oldScript =
-  let oldHashB   = C.serialiseToRawBytes oldHash
-      newHashB   = C.serialiseToRawBytes newHash
-      oldScriptB = C.serialiseToRawBytes oldScript
+  let oldHashB   = C.serialiseToCBOR oldHash
+      newHashB   = C.serialiseToCBOR newHash
+      oldScriptB = C.serialiseToCBOR oldScript
       newScriptB = replaceBS oldHashB newHashB oldScriptB
-  in C.deserialiseFromRawBytes (C.proxyToAsType Proxy) newScriptB
+  in C.deserialiseFromCBOR (C.proxyToAsType Proxy) newScriptB
 
 {-| Replace all occurrences of the first bytestring with the second
 bytestring in the third bytestring
@@ -70,18 +66,26 @@ Returns the hash of the new script if it is different
 (ie. if the script has been modified)
 -}
 replaceHashAnyLang ::
-  ( C.SerialiseAsRawBytes (Hash h1)
-  , C.SerialiseAsRawBytes (Hash h2)
-  , C.HasTypeProxy h1
-  , C.HasTypeProxy h2
-  ) => Hash h1 -> Hash h2 -> ScriptInAnyLang -> Either C.SerialiseAsRawBytesError (ScriptInAnyLang, Maybe ScriptHash)
+  ( C.SerialiseAsCBOR (Hash h1)
+  , C.SerialiseAsCBOR (Hash h2)
+  ) => Hash h1 -> Hash h2 -> ScriptInAnyLang -> Either DecoderError (ScriptInAnyLang, Maybe ScriptHash)
 replaceHashAnyLang oldHash newHash = \case
   C.ScriptInAnyLang C.SimpleScriptLanguage script        -> Right (C.ScriptInAnyLang C.SimpleScriptLanguage script, Nothing)
-  C.ScriptInAnyLang (C.PlutusScriptLanguage lang) script -> do
+  C.ScriptInAnyLang (C.PlutusScriptLanguage C.PlutusScriptV1) script -> do
     new <- replaceHash oldHash newHash script
-    let oldHash = C.hashScript script
-        newHash = C.hashScript new
-    pure (C.ScriptInAnyLang (C.PlutusScriptLanguage lang) new, guard (oldHash /= newHash) $> newHash)
+    let oldScriptHash = C.hashScript script
+        newScriptHash = C.hashScript new
+    pure (C.ScriptInAnyLang (C.PlutusScriptLanguage C.PlutusScriptV1) new, guard (oldScriptHash /= newScriptHash) $> newScriptHash)
+  C.ScriptInAnyLang (C.PlutusScriptLanguage C.PlutusScriptV2) script -> do
+    new <- replaceHash oldHash newHash script
+    let oldScriptHash = C.hashScript script
+        newScriptHash = C.hashScript new
+    pure (C.ScriptInAnyLang (C.PlutusScriptLanguage C.PlutusScriptV2) new, guard (oldScriptHash /= newScriptHash) $> newScriptHash)
+  C.ScriptInAnyLang (C.PlutusScriptLanguage C.PlutusScriptV3) script -> do
+    new <- replaceHash oldHash newHash script
+    let oldScriptHash = C.hashScript script
+        newScriptHash = C.hashScript new
+    pure (C.ScriptInAnyLang (C.PlutusScriptLanguage C.PlutusScriptV3) new, guard (oldScriptHash /= newScriptHash) $> newScriptHash)
 
 -- TODO
 -- 1. Download a full "spending transaction"
@@ -98,11 +102,11 @@ data FullTx =
     deriving stock (Eq, Show, Generic)
     -- deriving anyclass (ToJSON, FromJSON)
 
-{-| Download the full transaction
--}
--- TODO: Move to blockfrost package
-resolveTx :: C.TxId -> m FullTx
-resolveTx = undefined
+-- {-| Download the full transaction
+-- -}
+-- -- TODO: Move to blockfrost package
+-- resolveTx :: C.TxId -> m FullTx
+-- resolveTx = undefined
 
 -- 2. Adjust the reference scripts with replaceHash (this effectively changes the StablecoinParams)
 
